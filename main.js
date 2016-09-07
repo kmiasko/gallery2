3d81bc0ba882e008750b91158e00589ea3a640d8
Krzysztof Miąsko <kmiasko@gmail.com>
2016-09-07 21:30:20 +0200

    handleFiles split

--- main.js
+++ main.js
@@ -6,19 +6,21 @@ var DRAGnDROP = (function() {
     handleFunction: log,
   };
 
-  var element,
-    files;
+  var element;
+  var files;
 
   function init(conf) {
+    // update config if object passed
     if (conf) {
       Object.assign(config, conf);
     }
-    element = document.createElement('div');
-    element.classList.add(config.elementClass);
+
+    // bind events to drag container
+    var parent = document.querySelector(config.parentContainer);
+    element = parent.querySelector('.' + config.elementClass);
     element.addEventListener("dragenter", dragenter, false);
     element.addEventListener("dragover", dragover, false);
     element.addEventListener("drop", drop, false);
-    render();
   }
 
   // nothing to do on dragenter
@@ -40,17 +42,12 @@ var DRAGnDROP = (function() {
     e.preventDefault();
     var dt = e.dataTransfer;
     files = dt.files;
+    // use passed function on dragged files
     config.handleFunction(files);
-    // handleFiles(files); 
   }
 
   function log(fileList) {
     console.log(fileList);
-  }
-
-  function render() {
-    var parent = document.querySelector(config.parentContainer);
-    parent.appendChild(element);
   }
 
   return {
@@ -77,125 +74,133 @@ var GALLERY = (function() {
     galleryArea: '#gallery'
   };
 
+  var parent;
+  var button;
+  var fileInput;
+  var gallery;
+
   // gallery init function
   // overvrite defaults in config
   function init(config) {
     if (config) {
       Object.assign(configuration, config);
     }
+
+    // initialize dom elements handles
+    parent = document.querySelector(configuration.parentContainer);
+    button = parent.querySelector(configuration.fileUploadButton);
+    fileInput = parent.querySelector(configuration.fileInput);
+    gallery = parent.querySelector(configuration.galleryArea);
+
     start();
   }
 
-  function start() {
+  // checks if the passed file is in accepted files array
+  function isAcceptedFile(file) {
+    // accepted file extensions
+    var acceptedRegExp = RegExp('.(' + configuration.acceptedFormats.join('|') + ')$', 'i');
+    return !acceptedRegExp.test(file);
+  }
 
-    var parent = document.querySelector(configuration.parentContainer);
-    var button = parent.querySelector(configuration.fileUploadButton);
+  // resizes image base on config width and height
+  function returnResizedImage(largeImage) {
 
-    // create file input field
-    var fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.name = 'files';
-    fileInput.accept = configuration.acceptedFormats.map(function(format) {
-      return '.' + format;
-    }).join(',');
-    fileInput.multiple = true;
-    parent.appendChild(fileInput);
+    // resize image with canvas
+    var oc = document.createElement('canvas');
+    var octx = oc.getContext('2d');
+    oc.width = configuration.imageSize.width;
+    oc.height = configuration.imageSize.height;
+    octx.drawImage(largeImage, 0, 0, oc.width, oc.height);
 
-    // create upload button
-    var button = document.createElement('button');
-    button.id = configuration.fileUploadButton.replace('#', '');
-    button.name = 'send';
-    button.textContent = 'Upload';
-    parent.appendChild(button);
+    // return small image data uri from canvas
+    var jpegUrl = oc.toDataURL("image/jpeg");
+    return jpegUrl;
+  }
 
-    // create drag area container
-    DRAGnDROP.init({ parentContainer: configuration.parentContainer, handleFunction: handleFiles });
+  function addAnchor(thumbnail) {
+    var anchor = document.createElement('a');
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      var image = e.target.parentNode;
+      // open new window with large image after anchor clicked
+      window.open(image.href, 'Image');
+    });
 
-    // create gallery container
-    var gallery = document.createElement('div');
-    gallery.id = configuration.galleryArea.replace('#', '');
-    parent.appendChild(gallery);
+    anchor.appendChild(thumbnail);
 
-    // main anchor and thumbnails creating function
-    function handleFiles(files) {
+    return anchor;
+  }
 
-      // get gallery container 
-      var gallery = document.querySelector(configuration.galleryArea);
+  function addSmallImg() {
+    var smallImg = document.createElement('img');
+    return smallImg;
+  }
 
-      // accepted file extensions
-      var acceptedFiles = RegExp('.(' + configuration.acceptedFormats.join('|') + ')$', 'i');
+  function addLargeImg(file) {
+    var largeImg = document.createElement('img');
+    largeImg.file = file;
 
-      for (var i = 0; i < files.length; i++) {
+    return largeImg;
+  }
 
-        if (!acceptedFiles.test(files[i].name)) {
-          continue;
-        }
+  // handles image upload
+  function addRender(anchor, largeImg, file) {
+    var reader = new FileReader();
+    reader.onload = (function(aImg, anchor) {
+      // return closure that accepts event
+      return function(e) {
+        // add uri to anchor href, and set large image data uri
+        anchor.href = e.target.result;
+        aImg.src = e.target.result;
+      };
+    })(largeImg, anchor);
+    reader.readAsDataURL(file);
+  }
 
-        // create elements for single anchor / thumb / large image
-        var anchor = document.createElement('a');
-        var largeImg = document.createElement('img');
-        var smallImg = document.createElement('img');
+  function createThumbnail(largeImage, smallImage) {
+    largeImage.onload = (function(smallImage) {
+      // return closure that accepts event and resizes large image
+      // to thumb with canvas (in returnResizedImage)
+      return (function(e) {
+        // assign data uri to thumbnail img element
+        smallImage.src = returnResizedImage(e.target);
+      });
+    })(smallImage);
+  }
 
-        largeImg.file = files[i];
-
-        // open new window with large image after anchor clicked
-        anchor.addEventListener('click', function(e) {
-          e.preventDefault();
-          var image = e.target.parentNode;
-          window.open(image.href, 'Image');
-        });
-
-        // append thumb to 
-        anchor.appendChild(smallImg);
-        gallery.appendChild(anchor);
-
-        var reader = new FileReader();
-        reader.onload = (function(aImg, anchor) {
-          // return closure that accepts event
-          return function(e) {
-            anchor.href = e.target.result;
-            aImg.src = e.target.result;
-          };
-        })(largeImg, anchor);
-        reader.readAsDataURL(files[i]);
-
-        // after large image is loaded create thumb
-        // pass small img container to onload function
-        largeImg.onload = (function(smallImg) {
-          // return closure that accepts event
-          return (function(e) {
-
-            // resize image with canvas
-            var oc = document.createElement('canvas');
-            var octx = oc.getContext('2d');
-            oc.width = configuration.imageSize.width;
-            oc.height = configuration.imageSize.height;
-            octx.drawImage(e.target, 0, 0, oc.width, oc.height);
-
-            // return small image data uri from canvas
-            var jpegUrl = oc.toDataURL("image/jpeg");
-
-            // assign data uri to thumbnail img element
-            smallImg.src = jpegUrl;
-          });
-        })(smallImg);
+  // handle uploaded files
+  function handleFiles(files) {
+    for (var i = 0; i < files.length; i++) {
+      if (!isAcceptedFile(files[i])) {
+        continue;
       }
+      // create elements for single anchor / thumb / large image
+      var largeImg = addLargeImg(files[i]);
+      var smallImg = addSmallImg();
+      var anchor = addAnchor(smallImg);
+      // append elements to gallery container
+      gallery.appendChild(anchor);
+      addRender(anchor, largeImg, files[i]);
+      // after large image is loaded create thumb
+      // pass small img container to onload function
+      createThumbnail(largeImg, smallImg);
     }
+  }
 
+  function start() {
+    DRAGnDROP.init({ parentContainer: configuration.parentContainer, handleFunction: handleFiles });
     // upload files event for button
     button.addEventListener('click', function(e) {
       e.preventDefault();
       gallery.innerHTML = '';
       handleFiles(fileInput.files);
     });
-
     document.removeEventListener('DOMContentLoaded', start);
   }
 
   return {
     init: init,
   };
-
 })();
 
 GALLERY.init();
